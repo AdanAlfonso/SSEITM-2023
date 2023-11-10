63fd299b310a0e828729d5f1e138c9ee4b9dcae4
IamGosth <143956407+IamGosth@users.noreply.github.com>
2023-11-09 22:17:16 -0600

    holi
    checado y funcionando

--- proyectoMale - copia/scripts/encuestaProgreso.js
+++ proyectoMale - copia/scripts/encuestaProgreso.js
@@ -1,5 +1,5 @@
 let pagina = 1; // Página actual (inicialmente, página 1)
-    
+
 // Función para cambiar al contenido de la siguiente página
 function siguientePagina() {
     if (pagina < 5) { // Cambia el número según el número total de páginas
@@ -257,11 +257,11 @@ function actualizarContenido() {
     // Actualizar contenido del cuestionario
     if (pagina === 1) {
         cuestionario.innerHTML = `
-
+        <form action="pagina.php" method="GET" style="margin:3%; background-color:withe; padding: 3%; margin-top: 0px; margin-left: -50px; ">
 <div class="content-wrapper">
     <div class="text-content">
     <img src="https://sic.cultura.gob.mx/images/65091" class="side-image">
-    <div class="linea"><p>___________________________________________________________________________________________________________________</p></div>
+    <div class="linea"><p></p></div>
     <h1>Preeliminar</h1>
 
         <p>Instituto tecnologico de Minatitlán</p>
@@ -288,7 +288,7 @@ function actualizarContenido() {
         `;
     } else if (pagina === 2) {
         cuestionario.innerHTML = `
-        
+        <form action="pagina.php" method="GET" style="margin:3%; background-color:withe; padding: 3%; margin-top: -20px; margin-left: -50px; ">
             <div class="content-wrapper">
     <div class="text-content">
     <img src="https://sic.cultura.gob.mx/images/65091" class="side-image">
@@ -311,35 +311,135 @@ function actualizarContenido() {
     else if (pagina === 3) {
         cuestionario.innerHTML = `
         
-            <div class="content-wrapper">
-    <div class="text-content">
-    <img src="https://sic.cultura.gob.mx/images/65091" class="side-image">
-    <h1>Perfil y calidad</h1>
-        <p>Instituto tecnologico de minatitlán</p>
-        <p>Fecha:</p>
-        <p>Estimado Egresado:</p>
-        <p>Los servicios educativos de este instituto tecnologico debe estar en mejora continua para
-            asegurar la pertinencia de los conocimientos adquiridos y mejorar sistematicamente, para
-            colaborar en la formacion integral de nuestros alumnos.
-            para esto es indispensable tomarte en cuenta como factor de cambios y reformas, por lo que por
-            este medio solicitamos tu valiosa
-            participacion y cooperacion en esta investigacion del.</p>
-        <p>Seguimiento de Egresados, que nos permitirá obtener informacion valiosa para analizar la
-            problematica del mercado laboral y sus
-            caracteristicas, asi como las competencias laborales de nuestros egresados.
-            Las respuestas del cuestionario anexo seran tratadas con absoluta confidencialidad y con fines
-            meramente estatidiscos.</p>
-        <p>con nuestro agradecimiento por tu cooperación, recibe un cordial saludo.</p>
-        <p>Atentamente</p>
-        <p>SERGIO FERNANDO GARIBAY ARMENTA</p>
-        <p>Director del plantel</p>
-    </div>
+        <div class="container-fluid" >
+		<form action="pagina.php" method="GET" style="margin: 13%; background-color:#F2F2F2; padding: 0%; margin-left: -10px;">
+			<!-- primera fila -->
+			<div class="form-group">
+				<div class="row">
+					
+					<!-- primera columna -->
+					<div class="col-md-3">
+						<label style="color:#203c6c" for="validationTooltip01">Nombre</label>
+						<input  class="form-control"type="text" name="nombres" pattern="[A-Za-z]+" title="Solo se permiten letras" placeholder="Ej. Martina"> 
+					</div>
+					<!-- segunda columna -->
+					<div class="col-md-3">
+						<label style="color:#203c6c" for="validationTooltip01">Apellido Paterno</label>
+						<input  class="form-control"type="text" name="apellidoM" pattern="[A-Za-z]+" title="Solo se permiten letras" placeholder="Ej. Hernandez">
+					</div>
+					<!-- tercera columna -->
+					<div class="col-md-3">
+						<label style="color:#203c6c" for="validationTooltip01">Apellido Materno</label>
+						<input  class="form-control"type="text" name="apellidoP" pattern="[A-Za-z]+" title="Solo se permiten letras" placeholder="Ej. Lopez">
+					</div>
+					<div class="col-md-3">
+						<label style="color:#203c6c" for="validationTooltip01">Numero de control</label>
+						<input  class="form-control"type="number" name="Ncontrol" min="8" max="8"  placeholder="Ej. 18230752" >
+						
+					</div>
+
+					
+				</div>
+			</div>
+			<!-- segunda fila -->
+			<div class="form-group">
+				<div class="row">
+					<!-- primera columna -->
+					<div class="col-md-3">
+						<label style="color:#203c6c" for="validationTooltip01">Feha de nacimiento</label>
+						<input  class="form-control"type="date" name="nacimiento" placeholder="Fecha de nacimiento">
+					</div>
+					<!-- segunda columna -->
+					<div class="col-md-3" >
+						<label style="color:#203c6c" for="validationTooltip01">CURP</label>
+						<input  class="form-control"type="text" name="curp" placeholder="Ej. FEPM890204HASRRN08">
+					</div>
+					<!-- tercera columna -->
+					<div class="col-md-3">
+						<label style="color:#203c6c"  for="validationTooltip01">Sexo</label>
+						<select class="form-control" >
+							<option value="" selected disabled>Selecciona una opción</option>
+							<option>Femenino</option>
+							<option>Masculino</option>
+						</select>
+					</div>
+					<!-- cuarta columna -->
+					
+					<div class="col-md-3">	
+						<label style="color:#203c6c" for="validationTooltip01">Estado civil</label>
+						<select class="form-control" >
+							<option value="" selected disabled>Selecciona una opción</option>
+							<option>Soltero(a)</option>
+							<option>Casado(a)</option>
+							<option>Otro</option>
+						</select>
+						
+					</div>
+				</div>
+			</div>
+			<!-- tercera fila -->
+			<div class="form-group">
+				<div class="row">
+					<!-- primera columna -->
+					<div class="col-md-3">
+						<select  class="form-control">
+							<option>Registro civil</option>
+							<option>Tarjeta de identidad</option>
+							<option>Cedula</option>
+						</select>
+					</div>
+					<!-- segunda columna -->
+					<div class="col-md-3">
+						<input  class="form-control"type="number" name="ndocumento" placeholder="Numero de documentó">
+					</div>
+					<!-- tercera columna -->
+					<div class="col-md-3">
+						<input class="form-control" type="date" name="expedicion" placeholder="Expedición">
+					</div>
+				</div>
+			</div>
+
+				<!-- uarta fila -->
+			<div class="form-group">
+				<div class="row">
+					
+					<!-- primera columna -->
+					<div class="col-md-3">
+						<p>Nombre</p> 
+						<input  class="form-control"type="text" name="nombres" placeholder="Nombres"> 
+					</div>
+					<!-- segunda columna -->
+					<div class="col-md-3">
+						<p>Apellido Paterno</p> 
+						<input  class="form-control"type="text" name="apellidos" placeholder="Apellidos">
+					</div>
+					<!-- tercera columna -->
+					<div class="col-md-3">
+						<p>Apellido Materno</p> 
+						<input  class="form-control"type="date" name="nacimiento" placeholder="Fecha de nacimiento">
+					</div>
+					<div class="col-md-3">
+						<p>Numero de control</p> 
+						<input  class="form-control"type="date" name="nacimiento" placeholder="Fecha de nacimiento">
+					</div>
+
+					
+				</div>
+			</div>
+			<button class="btn btn-default" type="submit">Guardar</button>
+		</form>
+	</div>
         `;
     }
     else if (pagina === 4) {
         cuestionario.innerHTML = `
+        <form action="pagina.php" method="GET" style="margin: 13%; background-color:#F2F2F2; padding: 0%; margin-left: -10px;">
+            
         
-            <div class="content-wrapper">
+        
+        
+        
+        <div class="content-wrapper">
     <div class="text-content">
     <img src="https://sic.cultura.gob.mx/images/65091" class="side-image">
     <h1>Ubicación laboral</h1>
@@ -367,6 +467,13 @@ function actualizarContenido() {
     }
     else if (pagina === 5) {
         cuestionario.innerHTML = `
+        <form action="pagina.php" method="GET" style="margin: 13%; background-color:#F2F2F2; padding: 0%; margin-left: -10px;">
+
+
+
+
+
+
         
         <div class="content-wrapper">
         <div class="text-content">
@@ -395,29 +502,29 @@ function actualizarContenido() {
         `;
     }
 
-    if (pagina===1){
+    if (pagina === 1) {
         footer.innerHTML = `
         <span class="page-counter1">${pagina}/${totalPaginas}</span>
         <button id="goForward" class="button-forward" onclick="siguientePagina()"></button>
         `;
 
     }
-    else if (pagina===5){
+    else if (pagina === 5) {
         footer.innerHTML = `
         <button id="goBackward" class="button-backward" onclick="regresarPagina()"></button> 
         <span class="page-counter5">${pagina}/${totalPaginas}</span> 
         `;
     }
-    else{
+    else {
         footer.innerHTML = `
         <button id="goBackward" class="button-backward" onclick="regresarPagina()"></button>
         <span class="page-counter">${pagina}/${totalPaginas}</span>
         <button id="goForward" class="button-forward" onclick="siguientePagina()"></button>   
         `;
     }
-    
+
 }
 
-window.onload = function() {
-actualizarContenido();
+window.onload = function () {
+    actualizarContenido();
 };
